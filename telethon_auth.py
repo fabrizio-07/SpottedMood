6bdcda5fa2f789e00733eb60d4f12314527eed56
fabrizio-07 <CPRFRZ01B07C351V@studium.unict.it>
2025-07-09 13:13:09 +0200

    docs(telethon_auth): translated comments in english.

--- telethon_auth.py
+++ telethon_auth.py
@@ -7,25 +7,25 @@ phone_number = os.environ.get("PHONE_NUMBER")
 username = "SpottedMood"
 
 if not api_id or not api_hash or not phone_number:
-    raise ValueError("API_ID, API_HASH e PHONE_NUMBER devono essere settati come variabili d'ambiente")
+    raise ValueError("API_ID, API_HASH e PHONE_NUMBER must be set in .env file.")
 
 client = TelegramClient(username, api_id, api_hash)
 
 async def main():
     await client.connect()
-    print("Client connesso...")
+    print("Client connected...")
 
     if not await client.is_user_authorized():
         await client.send_code_request(phone_number)
-        code = input("Inserisci il codice ricevuto su Telegram: ")
+        code = input("Insert the Telegram OTP-code: ")
         await client.sign_in(phone_number, code)
     
     me = await client.get_me()
-    print("Autenticato come:", me.first_name, f"(@{me.username})")
+    print("Authenticated as:", me.first_name, f"(@{me.username})")
 
 with client:
     try:
         client.loop.run_until_complete(main())
     except Exception as e:
-        print("Errore: ", e)
+        print("Error: ", e)
 
