62acd230ae343666b3a11acbbbd47412fe38f36d
fabrizio-07 <CPRFRZ01B07C351V@studium.unict.it>
2025-07-14 11:41:41 +0200

    feat: add sentiment analysis to the bot.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import importlib
 import telethon_auth as ta
 import msg_extractor as me
+import sentiment
 from telethon import TelegramClient
 import os
 import asyncio
+from pysentimiento import create_analyzer
 
 importlib.reload(ta)
 importlib.reload(me)
+importlib.reload(sentiment)
 
 api_id = int(os.environ.get("API_ID"))
 api_hash = os.environ.get("API_HASH")
@@ -19,11 +22,15 @@ if not api_id or not api_hash or not phone_number:
 
 client = TelegramClient(username, api_id, api_hash)
 
+analyzer = create_analyzer(task="sentiment", lang="it")
+hate_analyzer = create_analyzer(task="hate_speech", lang="it")
+
 async def main():
     async with client:
         await ta.first_auth(client, phone_number)
-        await me.store_messages(client,spotted_id)
-        await client.run_until_disconnected()
+        await me.store_messages(client,spotted_id) #I haven't already managed store_messages and run_until_disconnected execution timing to be sure the bot won't stuck on these lines. 
+        await client.run_until_disconnected() 
+        await sentiment.sentiment_analyze(analyzer, hate_analyzer)
 
 if __name__ == "__main__":
     try:
---
+++ sentiment.py
@@ -0,0 +1,21 @@
+import json
+
+async def sentiment_analyze(analyzer, hate_analyzer):
+    with open('messages.json', 'r', encoding='utf-8') as msgs:
+        messages = json.load(msgs)
+
+    results = []
+
+    print("Analyzing today's messages...")
+    for msg in messages:
+        sentiment = analyzer.predict(msg['text'])
+        hatefulness = hate_analyzer.predict(msg['text'])
+
+        results.append({
+            'id': msg['id'],
+            'sentiment_probas': sentiment.probas,
+            'hate_probas': hatefulness.probas
+        })
+
+    with open('sentiment.json', 'w', encoding='utf-8') as sntm:
+        json.dump(results, sntm, indent=2, ensure_ascii=False)
