02635a02124cffd9dc74f08344b32f1314e135b7
fabrizio-07 <CPRFRZ01B07C351V@studium.unict.it>
2025-07-17 19:47:29 +0200

    feat: add start/stop handlers to manage message listener lifecycle, in order to make it work with the scheduler.

--- msg_extractor.py
+++ msg_extractor.py
@@ -1,25 +1,36 @@
 from telethon import events
 import json
 
+listener = None
+
 async def store_messages(client, chat_id):
-    
-    print("[MSG_EXTRACTOR] Waiting for new messages in Spotted DMI...")
+    global listener
     messages = []
 
-    @client.on(events.NewMessage(chats=chat_id))
-    async def handler(event):
+    if listener is not None:
+        return
+
+    print("[MSG_EXTRACTOR] Waiting for new messages in Spotted DMI...")
+
+    async def messages_listener(event):
         message = event.message
-        
-        if message and message.text:
+        if message and message.text and not message.text.startswith("by:"):
             json_message = {
                 "id": message.id,
                 "date": message.date.isoformat(),
                 "text": message.text,
             }
-            if not json_message["text"].startswith("by:"):
-                messages.append(json_message)
-                print(f"[MSG_EXTRACTOR] New incoming message: '{messages[-1]['text']}'")
+            messages.append(json_message)
+            print(f"[MSG_EXTRACTOR] New incoming message: '{json_message['text']}'")
+            with open("messages.json", "w", encoding="utf-8") as f:
+                json.dump(messages, f, ensure_ascii=False, indent=2)
 
-                with open("messages.json", "w", encoding="utf-8") as f:
-                    json.dump(messages, f, ensure_ascii=False, indent=2)
-            
+    listener = messages_listener
+    client.add_event_handler(listener, events.NewMessage(chats=chat_id))
+
+async def stop_store_messages(client):
+    global listener
+    if listener:
+        print("[MSG_EXTRACTOR] Stop listening messages")
+        client.remove_event_handler(listener)
+        listener = None
