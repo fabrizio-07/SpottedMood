f49f30167372d00056bd1a9ba5d68d07411f693c
fabrizio-07 <CPRFRZ01B07C351V@studium.unict.it>
2025-07-28 16:42:47 +0200

    fix(telethon_auth): now can use the 2FA to authenticate with Telegram client using Telethon.

--- telethon_auth.py
+++ telethon_auth.py
@@ -1,11 +1,17 @@
+from telethon.errors import SessionPasswordNeededError
+
 async def first_auth(client,phone_number):
     await client.connect()
     print("[TELETHON_AUTH] Client connected...")
 
     if not await client.is_user_authorized():
         await client.send_code_request(phone_number)
-        code = input("[TELETHON_AUTH] Insert the Telegram OTP-code: ")
-        await client.sign_in(phone_number, code)
+        try:
+            code = input("[TELETHON_AUTH] Insert the Telegram OTP-code: ")
+            await client.sign_in(phone_number, code)
+        except SessionPasswordNeededError:
+            password = input("[TELETHON_AUTH] Two-step verification enabled. Please enter your password: ")
+            await client.sign_in(password=password)
     
     me = await client.get_me()
     print("[TELETHON_AUTH] Authenticated as:", me.first_name, f"(@{me.username})")
